812a8f749d4413242dcc81749b65bac7bee2388b
Khan <nguyenhoangkhan210@gmail.com>
2024-04-16 17:30:23 +0700

    update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -52,6 +52,7 @@ class UserController extends Controller
             $user = auth()->userOrFail();
 
             $validator = Validator::make($request->all(), [
+                'old_password' => 'required|string',
                 'new_password' => 'required|string|min:8|max:200',
             ], userValidatorMessages());
 
