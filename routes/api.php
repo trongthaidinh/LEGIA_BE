8376da7822a3b200110710151a7d003257422532
Khan <nguyenhoangkhan210@gmail.com>
2024-06-19 14:47:08 +0700

    update FriendshipController.php and api.php

--- routes/api.php
+++ routes/api.php
@@ -84,6 +84,7 @@ Route::group([
     Route::get('get-accepted-list', [FriendshipController::class, 'getAcceptedList']);
     Route::get('get-pending-list', [FriendshipController::class, 'getPendingList']);
     Route::get('user/{user_id}', [FriendshipController::class, 'getFriendListOfUser']);
+    Route::get('user/status/{user_id}', [FriendshipController::class, 'getFriendRequestStatus']);
     Route::post('add/{friend}', [FriendshipController::class, 'add']);
     Route::patch('accept/{id}', [FriendshipController::class, 'accept']);
     Route::delete('{id}', [FriendshipController::class, 'delete']);
