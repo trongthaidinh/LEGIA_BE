97be7a0cab5bd822f41b64121f7b23c4fdd28a02
Khan <nguyenhoangkhan210@gmail.com>
2024-08-01 14:06:03 +0700

    update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -89,7 +89,7 @@ class UserController extends Controller
         try{
             $user = auth()->userOrFail();
 
-            $dataUpdate = $request->except(['is_verified', 'role', 'password', 'email', 'phone_number', 'avatar', 'relationship_status']);
+            $dataUpdate = $request->except(['is_verified', 'role', 'password', 'email', 'phone_number', 'avatar']);
 
             $validator = Validator::make($dataUpdate, [
                 'first_name' => 'nullable|max:30',
